4fd35fe0e0818a1304ef8cd468ed10da8111324c
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-27 21:48:43 -0600

    creating methods

--- appes6.js
+++ appes6.js
@@ -13,7 +13,24 @@ class UI{
     }
 
     showAlert(message, className){
+//create div
+const div= document.createElement('div');
 
+//add classes
+div.className = `alert ${className}`;
+//add text
+div.appendChild(document.createTextNode(message));
+//get parent
+const container = document.querySelector('.container');
+//get form
+const form = document.querySelector('#book-form');
+//insert error label
+container.insertBefore(div, form);
+//timeout 
+setTimeout(function(){
+    document.querySelector('.alert').remove();
+}, 3000
+);
     }
 
     removeBookFromList(target){
